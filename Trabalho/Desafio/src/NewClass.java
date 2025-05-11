fa475850509f82cf54cdf81b9dd491719ba45cd7
Madu <eduardapaiva2008@gmail.com>
2025-05-10 22:43:02 -0300

    Add files via upload

---
+++ Trabalho/Desafio/src/NewClass.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author FELIPE
+ */
+public class NewClass {
+    private String texto;
+    private String chave;
+    
+    public void setTexto (String _texto) {texto = _texto;}
+    public void setChave (String _chave) {chave = _chave;}     
+    
+    public String getTexto() {return texto;}
+    public String getChave() {return chave;}
+    
+}
